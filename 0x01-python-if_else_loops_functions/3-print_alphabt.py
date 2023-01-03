d9428250ecc6696fc5300efcb81993dbd126cdfa
1LolaOla <funmiolay1@gmail.com>
2023-01-03 04:05:23 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if (chr(i) != 'q' and chr(i) != 'e'):
-        print("{0:c}".format(i), end"")
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
+        continue
+    print(chr(i).format(i), end='')
