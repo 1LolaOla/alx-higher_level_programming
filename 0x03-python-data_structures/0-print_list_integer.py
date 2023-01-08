2fc708bdace2c750453f3c9c70d19c723cd972db
1LolaOla <funmiolay1@gmail.com>
2023-01-08 14:49:46 -0800

    0-print_list_integer.py

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """prints integers in a list
+
+    Args:
+        my_list (list, optional): Prints integers in a list. Defaults to [].
+        """
+        for i in range(len(my_list)):
+            print('{:d}'.format(my_list[i]))
