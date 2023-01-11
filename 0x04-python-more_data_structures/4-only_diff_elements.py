c438f4c8d1e98dfac925cf649c9409b1a4003a57
1LolaOla <funmiolay1@gmail.com>
2023-01-11 10:23:03 -0800

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+    ""Function that returns a set of
+    all elements present in only one set
+
+    Args:
+        set_1 (set): set 1
+        set_2 (set): set 2
+
+    Returns:
+        set: set containing elements not intersecting
+    """
+
+    return set(set_1 - set_2).union(set_2 - set_1)
 
