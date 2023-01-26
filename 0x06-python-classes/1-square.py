3d91df3f09e865534581f9ea2a7d2e369dbdcb3c
1LolaOla <funmiolay1@gmail.com>
2023-01-26 03:54:28 -0800

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,14 +5,13 @@
 class Square:
      """Represents a square
      Attributes:
-      __size (int): size of a side of the square
-      """
-      def __init__(self, size):
-           """Initializes a square
-           Args:
-                size (int): size of a side of the square
-           Returns: None
-           """
-
-           self.__size = size
+         __size (int): size of a side of the square
+     """
+     def __init__(self, size):
+         """Initializes a square
+         Args:
+             size (int): size of a side of the square
+         Returns: None
+         """
+         self.__size = size
 
