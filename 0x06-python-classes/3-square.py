daefaa85adbefa8ef964de7352e0152372fce750
1LolaOla <funmiolay1@gmail.com>
2023-01-29 01:46:00 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-"""
-A module that sets size of square as private field
+""" defines a square """
 
-"""
 
 class Square:
-    """Square class
+    """ square with private instance attribute size """
 
-     private instance field(s) : size
+    def __init__(self, size=0):
+        """
+        initializes square
+        Args:
+            size: size of side of square
+        """
 
-     methods: __init__
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+           else:
+               self.__size = size
+        else:
+            raise TypeError('size must be an integer')
 
-     """
+    def area(self):
+        """
+        finds area of square
+        Returns:
+            the area of the square
+        """
 
-     def __init__(self, size=0):
-         """initializes the class
-
-         Args:
-             size (int, optional): size of square. Defaults to 0.
-         """
-         if (type(size) is not int):
-            raise TypeError("size must be an integer")
-         elif (size < 0):
-              raise ValueError("size must be >= 0")
-         else:
-             self.__size = size
-
-     def area(self):
-         """Get area of square instance
-
-         Returns:
-            int: area of square
-         """
-         return self.__size ** 2
+        return self.__size ** 2
