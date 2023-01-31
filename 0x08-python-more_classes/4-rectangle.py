1991217bccac4aae399749e16bbcc2615b39b905
1LolaOla <funmiolay1@gmail.com>
2023-01-30 17:53:33 -0800

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -54,9 +54,9 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
-                    for j in range(self.__height))
+                                for j in range(self.__height))
         return string
 
      def __repr__(self):
          """returns a string representation of the rectangle for reproduction"""
-         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+         return "Rectangle({:d}, :{:d})".format(self.__width, self.__height)
