00d7c1cf8cd0d7fb7ad8cfbdb03194de1c6057a8
1LolaOla <funmiolay1@gmail.com>
2023-01-30 19:00:28 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -58,7 +58,7 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
-                    for j in range(self.__height))
+                                for j in range(self.__height))
         return string
 
     def __repr__(self):
