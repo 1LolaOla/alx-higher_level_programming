ad46d0c289c1dd21577d8e748f30e4701b09ffea
1LolaOla <funmiolay1@gmail.com>
2023-04-12 10:44:39 -0700

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2]);
+
+function factorial(n) {
+if (isNaN(n)) {
+return 1;
+}
+if (n === 0) {
+return 1;
+}
+return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
