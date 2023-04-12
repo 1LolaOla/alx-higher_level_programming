57998b708d41e0a77727741ab69b29c8603ce07c
1LolaOla <funmiolay1@gmail.com>
2023-04-12 05:37:45 -0700

    8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+'use strict';
+console.log('Hello from ChatGPT!');
+const arg = process.argv[2];
+const size = parseInt(arg);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
