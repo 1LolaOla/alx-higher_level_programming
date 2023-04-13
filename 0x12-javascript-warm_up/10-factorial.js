47e4b1787c3e8090f6e79b01fe8cdfad2b0e317b
1LolaOla <funmiolay1@gmail.com>
2023-04-12 18:20:25 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+const num = Number(process.argv[2]);
+function fact (n) {
+  if (!n || n === 0) { return (1); }
+  return n * fact(n - 1);
 }
-
-console.log(factorial(Number(process.argv[2])));
+console.log(fact(num));
--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-if (args.length < 2) {
-console.log(0);
-} else {
-const sortedArgs = args.sort((a, b) => b - a);
-console.log(sortedArgs[1]);
-}
