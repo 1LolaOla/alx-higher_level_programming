45f2ab5606dff5e69e81019ead784625ae4babec
1LolaOla <funmiolay1@gmail.com>
2023-04-13 07:06:05 -0700

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let reversedList = [];
+  const reversedList = [];
+  const lastIndex = list.length - 1;
 
-  for (let i = list.length - 1; i >= 0; i--) {
+  for (let i = lastIndex; i >= 0; i--) {
     reversedList.push(list[i]);
   }
 
