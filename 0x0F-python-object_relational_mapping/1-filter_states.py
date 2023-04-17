3ea3d410a5cc1156e7c2601b346856b788930d1d
1LolaOla <funmiolay1@gmail.com>
2023-04-17 08:32:38 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Select States Module"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_host = "localhost"
+
+    db = MySQLdb.connect(user=db_username, password=db_password,
+                         host=db_host, database=db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+
+    for i in rows:
+        print(i)
+    cursor.close()
+    db.close()
