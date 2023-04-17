dcf5ea5a5249d5bdcaf9091fcfe0f2076f8cfd05
1LolaOla <funmiolay1@gmail.com>
2023-04-17 12:23:13 -0700

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Select States Module"""
+import sys
+import MySQLdb
+if __name__ == '__main__':
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+    db_host = "localhost"
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(user=db_username, password=db_password,
+                         host=db_host, database=db_name)
+    cursor = db.cursor()
+    sqlquery = "SELECT * FROM states WHERE name LIKE\
+         BINARY {} ORDER BY id ASC".format('%s')
+    cursor.execute(sqlquery, (state_name_searched,))
+    rows = cursor.fetchall()
+    for i in rows:
+        print(i)
+    cursor.close()
+    db.close()
