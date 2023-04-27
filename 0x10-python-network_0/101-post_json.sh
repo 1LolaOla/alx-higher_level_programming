95f5e6ebd7416f2c7fa1fa464d37043bac87b26f
1LolaOla <funmiolay1@gmail.com>
2023-04-27 11:54:46 -0700

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-# Get the URL and JSON file from command line arguments
-URL="$1"
-JSON_FILE="$2"
-
-# Check if the JSON file is valid
-if ! jq '.' "$JSON_FILE" >/dev/null 2>&1; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send the POST request with the contents of the JSON file in the body
-RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "@$JSON_FILE" "$URL")
-
-# Display the body of the response
-echo "$RESPONSE"
+# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
