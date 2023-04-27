c3792cb4ee486799a331a49d606fd05b05a80d8f
1LolaOla <funmiolay1@gmail.com>
2023-04-27 12:07:24 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a request to 0.0.0.0:5000/catch_me and causes the server to respond with "You got me!"
-
-curl -s -L -X PUT "0.0.0.0:5000/catch_me" -H "Origin: HolbertonSchool" -d "user_id=98"
+# makes a req to 0.0.0.0:5000/catch_me for a response "You got me!"
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me"
