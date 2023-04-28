27b3d715b53d63a9bdf17c5ef3d33d45fc4034e2
1LolaOla <funmiolay1@gmail.com>
2023-04-27 17:29:19 -0700

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
