f25ced89ba3065e1c14b80b58dd68c04b1705e28
1LolaOla <funmiolay1@gmail.com>
2023-05-23 01:58:20 -0700

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+
+    const completedTasksByUser = {};
+
+    tasks.forEach((task) => {
+      if (task.completed) {
+        if (completedTasksByUser[task.userId]) {
+          completedTasksByUser[task.userId]++;
+        } else {
+          completedTasksByUser[task.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
